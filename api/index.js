88481207f81b7c60c7ba1446f11d8b51275667ef
Aditya <adityag7522@gmail.com>
2024-12-15 12:35:22 +0530

    asdsad

--- api/index.js
+++ api/index.js
@@ -72,6 +72,9 @@ app.post('/saveLocation', async (req, res) => {
     }
 });
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
